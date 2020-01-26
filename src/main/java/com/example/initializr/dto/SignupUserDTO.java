d1f597771013977490906a70124d96b72e3ef1b6
Brian Froschauer <bafroschauer@gmail.com>
2020-01-25 23:51:14 -0300

    feat(dto): add validators

--- src/main/java/com/example/initializr/dto/SignupUserDTO.java
+++ src/main/java/com/example/initializr/dto/SignupUserDTO.java
@@ -4,13 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import static com.example.initializr.util.Patterns.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupUserDTO {
 
+    @NotNull
+    @Size(min = 2, max = 20)
     private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 20)
     private String lastName;
+
+    @Pattern(regexp = PATTERN_USERNAME)
     private String username;
+
+    @Pattern(regexp = PATTERN_PASSWORD)
     private String password;
 }
--- src/main/java/com/example/initializr/dto/UpdateUserDTO.java
+++ src/main/java/com/example/initializr/dto/UpdateUserDTO.java
@@ -4,11 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserDTO {
 
+    @NotNull
+    @Size(min = 2, max = 20)
     private String lastName;
+
+    @NotNull
+    @Size(min = 2, max = 20)
     private String username;
 }
