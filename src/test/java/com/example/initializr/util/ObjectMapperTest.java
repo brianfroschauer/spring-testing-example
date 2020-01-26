d6443a0fcad182f430365dc31c81956601d5b2f6
Brian Froschauer <bafroschauer@gmail.com>
2020-01-25 23:52:51 -0300

    test(ObjectMapper): add object mapper test

--- src/test/java/com/example/initializr/util/ObjectMapperTest.java
+++ src/test/java/com/example/initializr/util/ObjectMapperTest.java
@@ -1,8 +1,45 @@
 package com.example.initializr.util;
 
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import com.example.initializr.dto.UserDTO;
+import com.example.initializr.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(MockitoJUnitRunner.class)
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class ObjectMapperTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("Given user, when map to user DTO, then return user DTO")
+    public void givenUser_whenMapToUserDTO_thenReturnUserDTO() {
+        final User user = new User();
+        user.setId(1L);
+        user.setFirstName("firstName");
+        user.setLastName("lastName");
+        user.setUsername("username");
+        user.setPassword("password");
+
+        final UserDTO userDTO = objectMapper.map(user, UserDTO.class);
+        assertNotNull(userDTO);
+        assertEquals("firstName", userDTO.getFirstName());
+        assertEquals("lastName", userDTO.getLastName());
+        assertEquals("username", userDTO.getUsername());
+    }
+
+    @Test
+    @DisplayName("Given user list, when map to user DTO, then return list of user DTO")
+    public void givenUserList_whenMapToUserDTO_thenReturnUserListDTO() {
+        final List<UserDTO> users = objectMapper.map(Arrays.asList(new User(), new User()), UserDTO.class);
+        assertNotNull(users);
+        assertEquals(2, users.size());
+    }
 }
