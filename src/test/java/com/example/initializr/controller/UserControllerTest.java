3af6ab3164305b1cfa99fb5f7c6da3f2b4a65b6f
Brian Froschauer <bafroschauer@gmail.com>
2020-01-25 23:51:54 -0300

    test(controller): add controller tests

--- src/test/java/com/example/initializr/controller/UserControllerTest.java
+++ src/test/java/com/example/initializr/controller/UserControllerTest.java
@@ -2,7 +2,12 @@ package com.example.initializr.controller;
 
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
 public class UserControllerTest {
 }
